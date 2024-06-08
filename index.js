b5e807973e31ddc5efba92ec2d77a97cf05e3218
Maryam faizan <118754207+maryamfaizan53@users.noreply.github.com>
2024-06-09 01:46:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+let c_Value = {
+    PKR: 278.40,
+    UAE: 3.67,
+    USD: 1,
+};
+let ans = await inquirer.prompt([{
+        type: "list",
+        name: "from",
+        message: "converting from",
+        choices: ["PKR", "UAE", "USD"]
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "converting to",
+        choices: ["PKR", "UAE", "USD"]
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Your amount to convert"
+    }
+]);
+console.log(c_Value[ans.to] / c_Value[ans.from] * ans.amount);
